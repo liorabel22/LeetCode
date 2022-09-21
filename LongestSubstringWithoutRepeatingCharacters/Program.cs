55ef43987789bbd75ddb5cabadc42e4e564b5f8b
lior.abel <liorabel22@gmail.com>
2022-09-21 19:11:31 +0300

    longest substring without repeating characters

---
+++ LongestSubstringWithoutRepeatingCharacters/Program.cs
@@ -0,0 +1,7 @@
+﻿using LongestSubstringWithoutRepeatingCharacters;
+
+Solution sol = new Solution();
+Console.WriteLine($"\"abcabcbb\" - expected 3 and got: {sol.LengthOfLongestSubstring("abcabcbb")}");
+Console.WriteLine($"\" \" - expected 1 and got: {sol.LengthOfLongestSubstring(" ")}");
+Console.WriteLine($"\"\" - expected 0 and got: {sol.LengthOfLongestSubstring("")}");
+Console.WriteLine($"\"dvdf\" - expected 3 and got: {sol.LengthOfLongestSubstring("dvdf")}");
---
+++ LongestSubstringWithoutRepeatingCharacters/Solution.cs
@@ -0,0 +1,48 @@
+﻿namespace LongestSubstringWithoutRepeatingCharacters;
+
+public class Solution
+{
+    int[] lut = new int[256];
+    public int LengthOfLongestSubstring(string s)
+    {
+        int maxSum = 0;
+        int currentSum = 0;
+
+
+        InitialLut();
+        for (int i = 0; i < s.Length; ++i)
+        {
+            for (int j = i; j < s.Length; ++j)
+            {
+                lut[s[j]]++;
+                if (lut[s[j]] > 1)
+                {
+                    if (currentSum > maxSum)
+                    {
+                        maxSum = currentSum;
+                    }
+                    currentSum = 0;
+                    InitialLut();
+                    lut[s[j]]++;
+                }
+                currentSum++;
+                if (currentSum > maxSum)
+                {
+                    maxSum = currentSum;
+                }
+            }
+            currentSum = 0;
+            InitialLut();
+        }
+
+        return maxSum;
+    }
+
+    private void InitialLut()
+    {
+        for (int i = 0; i < 256; ++i)
+        {
+            lut[i] = 0;
+        }
+    }
+}

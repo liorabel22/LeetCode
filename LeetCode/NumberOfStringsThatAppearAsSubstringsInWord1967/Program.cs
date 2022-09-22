c44be58d259c0750228f124ecac1cffabc54549f
lior.abel <liorabel22@gmail.com>
2022-09-22 10:01:45 +0300

    Number of Strings That Appear as Substrings in Word - simple solution

--- LeetCode/NumberOfStringsThatAppearAsSubstringsInWord1967/Program.cs
+++ LeetCode/NumberOfStringsThatAppearAsSubstringsInWord1967/Program.cs
@@ -1,2 +1,9 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Solution solution = new Solution();
+
+string[] patterns1 = {"a", "abc", "bc", "d"};
+string[] patterns2 = { "a", "b", "c" };
+string[] patterns3 = { "a", "a", "a" };
+
+Console.WriteLine($"patterns = [\"a\",\"abc\",\"bc\",\"d\"], word = \"abc\" - expected: 3 - got: {solution.NumOfStrings(patterns1, "abc")}");
+Console.WriteLine($"patterns = [\"a\",\"b\",\"c\"], word = \"aaaaabbbbb\" - expected: 2 - got: {solution.NumOfStrings(patterns2, "aaaaabbbbb")}");
+Console.WriteLine($"patterns = [\"a\",\"a\",\"a\"], word = \"ab\" - expected: 3 - got: {solution.NumOfStrings(patterns3, "ab")}");
--- LeetCode/NumberOfStringsThatAppearAsSubstringsInWord1967/Solution.cs
+++ LeetCode/NumberOfStringsThatAppearAsSubstringsInWord1967/Solution.cs
@@ -2,6 +2,15 @@
 {
     public int NumOfStrings(string[] patterns, string word)
     {
+        int result = 0;
+        foreach(string pattern in patterns)
+        {
+            if (word.Contains(pattern))
+            {
+                ++result;
+            }
+        }
 
+        return result;
     }
 }

42c139839e8e228fea68da934bf51c531ab9ec87
lior.abel <liorabel22@gmail.com>
2022-09-24 09:37:27 +0300

    1680. Concatenation of Consecutive Binary Numbers - working but too slow

--- LeetCode/ConcatenationOfConsecutiveBinaryNumbers1680/Solution.cs
+++ LeetCode/ConcatenationOfConsecutiveBinaryNumbers1680/Solution.cs
@@ -4,54 +4,18 @@ public class Solution
 {
     public int ConcatenatedBinary(int n)
     {
-        long startNumber = 1;
-        bool enter = true;
-        List<long> list = new List<long>() { startNumber };
-        StringBuilder concatenatedBinary = new StringBuilder("1");
+        StringBuilder concatenatedBinary = new StringBuilder();
 
-        while (n >= list.Count)
+        for (int i = 1; i <= n; ++i)
         {
-            int sizeOfList = list.Count;
-            for (int i = 0; i < sizeOfList - 1; ++i)
-            {
-                if (n == list.Count)
-                {
-                    enter = false;
-                    break;
-                }
-                long numToAdd = startNumber + list[i];
-                list.Add(numToAdd);
-                concatenatedBinary.Append(numToAdd.ToString());
-            }
-            if (enter && (n > list.Count))
-            {
-                startNumber *= 10;
-                list.Add(startNumber);
-                concatenatedBinary.Append(startNumber.ToString());
-            }
-            else
-            {
-                break;
-            }
+            long num = 0;
+            concatenatedBinary.Append(Convert.ToString(i, toBase: 2));
+            num = Convert.ToInt64(concatenatedBinary.ToString(), fromBase: 2) % (int)(Math.Pow(10, 9) + 7);
+            concatenatedBinary.Clear();
+            concatenatedBinary.Append(Convert.ToString(num, toBase: 2));
         }
 
 
-        
-        return (int)((BinaryToDecimal(concatenatedBinary) % (long)(Math.Pow(10, 9) + 7)));
-    }
-
-    private long BinaryToDecimal(StringBuilder binary)
-    {
-        long result = 0;
-        long multi = 1;
-
-        while (binary.Length > 0)
-        {
-            result = result + (int.Parse(binary[^1].ToString()) * multi);
-            multi *= 2;
-            binary.Remove(binary.Length - 1, 1);
-        }
-
-        return result;
+        return (int)(Convert.ToInt64(concatenatedBinary.ToString(), fromBase: 2) % (int)(Math.Pow(10, 9) + 7));
     }
 }
